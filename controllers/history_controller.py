0487e3cf5ba7891d2d972cc19cde8bb3789b6461
minhssk1 <minhka002@gmail.com>
2026-01-26 00:45:59 +0700

    Create history_controller.py

---
+++ controllers/history_controller.py
@@ -0,0 +1,23 @@
+from database.db_connection import get_connection
+
+
+def view_borrow_history(member_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT b.title,
+               br.borrow_date,
+               br.due_date,
+               br.return_date,
+               br.status,
+               br.fine
+        FROM borrow_records br
+        JOIN books b ON br.book_id = b.id
+        WHERE br.member_id = ?
+        ORDER BY br.borrow_date DESC
+    """, (member_id,))
+
+    records = cursor.fetchall()
+    conn.close()
+    return records
