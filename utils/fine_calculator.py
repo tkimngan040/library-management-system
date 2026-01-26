9545dc8279cc05e29b3ee912c32d68dd8f73f1ae
duylong06 <bilongancom@gmail.com>
2026-01-27 00:27:42 +0700

    Update fine_calculator.py

--- utils/fine_calculator.py
+++ utils/fine_calculator.py
@@ -1,15 +1,16 @@
-from datetime import date
+from datetime import datetime
+import config
 
-FINE_PER_DAY = 5000  # 5,000 VND / day
-
-
-def calculate_fine(due_date, return_date):
-    """
-    Calculate overdue fine based on due date and actual return date
-    """
-    if return_date <= due_date:
+def calculate_overdue_fine(due_date_str, return_date_str=None):
+    if return_date_str is None:
+        return_date_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    due = datetime.strptime(due_date_str, '%Y-%m-%d %H:%M:%S')
+    ret = datetime.strptime(return_date_str, '%Y-%m-%d %H:%M:%S')
+    
+    if ret <= due:
         return 0, 0
-
-    overdue_days = (return_date - due_date).days
-    fine = overdue_days * FINE_PER_DAY
+    
+    overdue_days = (ret - due).days
+    fine = overdue_days * config.FINE_PER_DAY
     return overdue_days, fine
