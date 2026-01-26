8efe6000c6f8ddefe3c61f208da650f4ff4f5b3d
duylong06 <bilongancom@gmail.com>
2026-01-27 00:25:38 +0700

    Delete tests directory

--- tests/test_search.py
+++
@@ -1,9 +0,0 @@
-from controllers.search_controller import SearchController
-
-def test_search_book():
-    results = SearchController.search_books(keyword="Library")
-    assert isinstance(results, list)
-
-def test_sort_books():
-    results = SearchController.search_books(sort_by="title")
-    assert results == sorted(results, key=lambda x: x["title"])
