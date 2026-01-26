6f46849d6f0e616a25700041f1c3df16a4b2fee1
duylong06 <bilongancom@gmail.com>
2026-01-26 08:11:48 +0700

    Delete test_auth.py

--- test_auth.py
+++
@@ -1,7 +0,0 @@
-from controllers.auth_controller import login, logout, change_password, get_current_user
-user = login()
-
-if user:
-    print("Current user:", get_current_user())
-    change_password()
-    logout()
