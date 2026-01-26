edd251148430d24f8537edc36c3bc644394de100
duylong06 <bilongancom@gmail.com>
2026-01-27 00:07:14 +0700

    Update display_controller.py

--- controllers/display_controller.py
+++ controllers/display_controller.py
@@ -1,48 +1,14 @@
-from database.databaseConnect.db_connect import get_connection
+from models.book import Book
 
 class DisplayController:
-    """
-    Hiển thị danh sách & chi tiết sách
-    """
-
     @staticmethod
     def get_all_books():
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM books")
-        rows = cursor.fetchall()
-        conn.close()
-
-        books = []
-        for r in rows:
-            books.append({
-                "book_id": r[0],
-                "title": r[1],
-                "author": r[2],
-                "category": r[3],
-                "total_copies": r[4],
-                "available_copies": r[5],
-                "description": r[6]
-            })
-        return books
-
+        return Book.get_all_books()
+    
     @staticmethod
     def get_book_details(book_id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM books WHERE book_id=?", (book_id,))
-        r = cursor.fetchone()
-        conn.close()
-
-        if not r:
-            return None
-
-        return {
-            "book_id": r[0],
-            "title": r[1],
-            "author": r[2],
-            "category": r[3],
-            "total_copies": r[4],
-            "available_copies": r[5],
-            "description": r[6]
-        }
+        return Book.get_book_by_id(book_id)
+    
+    @staticmethod
+    def get_categories():
+        return Book.get_categories()
