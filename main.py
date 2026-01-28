989a494dcb9af77c11d6b62705384fad9707e926
minhssk1 <minhka002@gmail.com>
2026-01-28 22:35:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from controllers.app_controller import AppController
 from database import init_db
-
-if __name__ == "__main__":
+if __name__ == " __main__ ":
     init_db()
     root = tk.Tk()
     root.title("Library Management System")
